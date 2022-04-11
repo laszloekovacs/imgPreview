a35d97b0e24312c62e226458b0a2c13eb5d642ef
laszloekovacs <laszloekovacs@gmail.com>
2022-04-11 16:33:39 +0200

    added more garbage mostly

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 const image = document.getElementById('image');
 
-image.addEventListener('input', (e) => {
-	for (let file of image.files) {
+function createPreviews(files) {
+	for (let file of files) {
 		const img = new Image();
 		img.src = window.URL.createObjectURL(file);
 		document.getElementById('app').appendChild(img);
 	}
+}
+
+image.addEventListener('input', (e) => {
+	createPreviews(file.files);
 });
+
+const dropzone = document.querySelector('.dropzone_main');
+
+function dropHandler(e) {
+	e.preventDefault();
+	console.log('dropped');
+
+	for (let item of e.dataTransfer.items) {
+		const data = item.getAsFile();
+		const img = new Image();
+		img.src = window.URL.createObjectURL(data);
+		document.getElementById('app').appendChild(img);
+	}
+}
+
+function dragOverHandler(e) {
+	e.preventDefault();
+	console.log('dragged over');
+}
+
+dropzone.addEventListener('drop', dropHandler);
+dropzone.addEventListener('dragover', dragOverHandler);
