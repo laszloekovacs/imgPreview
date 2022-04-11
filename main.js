666d4a6b6a85f248e37a33cf0e5a1887278671e0
laszloekovacs <laszloekovacs@gmail.com>
2022-04-11 16:57:21 +0200

    increased unreadability beyond repair

--- main.js
+++ main.js
@@ -14,6 +14,18 @@ image.addEventListener('input', (e) => {
 
 const dropzone = document.querySelector('.dropzone_main');
 
+function img_dragHandler(e) {
+	//e.preventDefault();
+	let img = new Image();
+	img.src = e.target.src;
+	//img.width = 40;
+	//img.height = 40;
+	e.dataTransfer.setDragImage(img, 10, 10);
+	console.log('dragging started');
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.dropEffect = 'move';
+}
+
 function dropHandler(e) {
 	e.preventDefault();
 	console.log('dropped');
@@ -21,6 +33,8 @@ function dropHandler(e) {
 	for (let item of e.dataTransfer.items) {
 		const data = item.getAsFile();
 		const img = new Image();
+		img.setAttribute('draggable', 'true');
+		img.addEventListener('dragstart', img_dragHandler);
 		img.src = window.URL.createObjectURL(data);
 		document.getElementById('app').appendChild(img);
 	}
@@ -33,3 +47,19 @@ function dragOverHandler(e) {
 
 dropzone.addEventListener('drop', dropHandler);
 dropzone.addEventListener('dragover', dragOverHandler);
+
+const dragEnterHandler = (e) => {
+	e.preventDefault();
+	const dialog = document.createElement('dialog');
+	document.body.appendChild(dialog);
+	dialog.show();
+	console.log('something');
+};
+
+const dragLeaveHandler = (e) => {
+	e.preventDefault();
+	console.log('leaving');
+};
+
+document.body.addEventListener('dragenter', dragEnterHandler);
+document.body.addEventListener('dragleave', dragLeaveHandler);
